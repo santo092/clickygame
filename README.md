71f90c79cbcdf73e5d8605add7545dff993ec737
Trixie Santos <alexisvillalpando@Tsmacbookpro.local>
2019-04-23 22:21:54 -0700

    push

---
+++ README.md
@@ -0,0 +1,16 @@
+# Clicky Game
+
+## Overview
+A game with a main goal of not clicking a previously clicked image. 
+
+## How It Works
+- Click an image.
+- Earn a point by clicking a different image once the images shuffle. 
+- Beat the highscore. 
+
+[Check out the App](https://mongosrc.herokuapp.com/)
+
+## Technologies Used
+- react
+- react-dom
+- react-scripts
---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+// import Title from "./components/Title";
+import friends from "./friends.json";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    score: 0,
+    highscore: 0
+  };
+
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({ highscore: this.state.score }, function () {
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.friends.forEach(friend => {
+      friend.count = 0;
+    });
+    alert(`Game Over :( \n score: ${this.state.score}`);
+    this.setState({ score: 0 });
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.friends.find((o, i) => {
+      if (o.id === id) {
+        if (friends[i].count === 0) {
+          friends[i].count = friends[i].count + 1;
+          this.setState({ score: this.state.score + 1 }, function () {
+            console.log(this.state.score);
+          });
+          this.state.friends.sort(() => Math.random() - 0.5)
+          return true;
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            clickCount={this.clickCount}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+
+
+export default App;
